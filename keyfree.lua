9b89375da8c51ad59440fa1039a5f3b4d1895557
Anonymous9x-oss <bopaldwi@gmail.com>
2026-04-08 18:47:54 +0700

    Yreup

--- keyfree.lua
+++ keyfree.lua
@@ -4,7 +4,7 @@ local VALID_KEY = "SS907-ANONYMOUS9x-KEY-FREE"
 --// GAME ROUTER
 local SCRIPTS = {
     [10449761463] = "https://raw.githubusercontent.com/Anonymous9x-oss/FullFreeScA9x/refs/heads/main/Maintsb.lua", -- game 1
-    [1234567890]  = "LINK_SCRIPT_GAME2",            -- game 2
+    [142823291]  = "https://raw.githubusercontent.com/Anonymous9x-oss/FullFreeScA9x/refs/heads/main/MainMM2.lua",            -- game 2
     [9876543210]  = "LINK_SCRIPT_GAME3"             -- game 3
 }
 
